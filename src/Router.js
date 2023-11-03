27ccc4b9837d817c81d472a1e4e3011501b85e7b
Raphael <rapha.saverys@gmail.com>
2023-11-03 12:10:36 +0100

    Create all page & add them to the router

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,17 @@
 import { Routes, Route } from "react-router-dom";
-import App from "./App";
+import Home from "./pages/Home";
+import Logement from "./pages/Logement";
+import About from "./pages/About";
+import Error404 from "./pages/Error404";
 
 
 const Router = () => {
     return (
         <Routes>
-            <Route path="/" element={<App />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/logement" element={<Logement />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<Error404 />} />
         </Routes>
     )
 }
---
+++ src/pages/Logement.jsx
@@ -0,0 +1,8 @@
+const Logement = () => {
+  return (
+    <div>
+      <h1>Logement</h1>
+    </div>
+  );
+};
+export default Logement;
