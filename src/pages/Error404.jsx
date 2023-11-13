0466085d5ffdfd4fa597ab717f55e24905a1a30a
Raphael <rapha.saverys@gmail.com>
2023-11-13 14:31:54 +0100

    created the 404 page

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,8 +1,18 @@
+import MainContainer from "../layouts/MainContainer";
+
 const Error404 = () => {
-    return (
-        <div>
-        <h1>404</h1>
-        </div>
-    );
-    }
-    export default Error404;
+  return (
+    <MainContainer className="error404">
+      <section className="error404__content">
+        <h1 className="error404__title">404</h1>
+        <p className="error404__description">
+          Oups! La page que vous demandez n'existe pas.
+        </p>
+        <a href="/" className="error404__link">
+          Retourner sur la page d'accueil
+        </a>
+      </section>
+    </MainContainer>
+  );
+};
+export default Error404;
